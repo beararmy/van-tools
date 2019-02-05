b5cc82de566f89283e644a8bd1e2c34846db0dc2
Stefan Harrington-Palmer <stef@bear.army>
2019-02-05 12:58:37 +0000

    Initial van climate page

---
+++ van.php
@@ -0,0 +1,29 @@
+<?php
+
+# First attempt at a page to neatly display the contents of batteries-climate-latest.csv as a webpage
+#
+
+$filename = "./batteries-climate-latest.csv";
+    $csv = array_map('str_getcsv', file($filename));
+    array_walk($csv, function(&$a) use ($csv) {
+      $a = array_combine($csv[0], $a);
+    });
+    array_shift($csv);
+
+$timestamp = $csv[0]['timestamp'];
+$temperature = $csv[0]['temperature_in_celsius'];
+$humidity = $csv[0]['relative_humidity'];
+
+echo "<html>
+<head>
+<meta http-equiv=refresh content=10 />
+<title>Van Climate</title>
+</head>
+<body>";
+echo "<h2>Current climate in the van:</h2>";
+echo "<b>Last updated at: </b>$timestamp<br>";
+echo "<b>Temperature is: </b>$temperature<br>";
+echo "<b>Humidity is: </b>$humidity<br>";
+echo "</body>
+</html>";
+?>
